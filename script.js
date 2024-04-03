46147e58fdff2e8da31f7f33f70b5754bd497080
jmiguelmangas <jmmangas@gmail.com>
2024-04-03 13:36:09 +0200

    FrontEnd Changed

--- script.js
+++ script.js
@@ -23,21 +23,26 @@ function capitalize(string) {
   return string[0].toUpperCase() + string.slice(1);
 }
 function mostrarDatosClima(response) {
-  const divDatosClima = document.getElementById("datosClima"); //cogemos el div datosClima del html
-  divDatosClima.innerHTML = ""; //reseteamos
+  const divHeaderClima = document.createElement("header");
+  const divFooterClima = document.createElement("footer");
+  const divDatosClima = document.getElementById("datosClima");
+  divDatosClima.innerHTML = "";
+  divDatosClima.appendChild(divHeaderClima);
+  divDatosClima.appendChild(divFooterClima);
   const cityName = response.name;
   const cityTemp = kelvinToCelsius(response.main.temp);
   const cityTempMin = kelvinToCelsius(response.main.temp_min);
   const cityTempMax = kelvinToCelsius(response.main.temp_max);
   const cityDesc = response.weather[0].description;
   const cityWeatherIcon = response.weather[0].icon;
+  const cityCountryInfo = response.sys.country;
   const cityTempTitulo = document.createElement("h2");
   const humidity = response.main.humidity;
   cityTempTitulo.setAttribute("id", "tituloTemp");
   cityTempTitulo.textContent = `${cityTemp} ºC`;
   const ciudadTitulo = document.createElement("h2");
   ciudadTitulo.setAttribute("id", "ciudadTitulo");
-  ciudadTitulo.textContent = cityName;
+  ciudadTitulo.textContent = `${cityName},${cityCountryInfo}`;
   const ciudadTempInfo = document.createElement("h3");
   ciudadTempInfo.setAttribute("id", "ciudadTempInfo");
   ciudadTempInfo.textContent = `La temperatura maxima del dia en ${cityName} será de ${cityTempMax} ºC y la minima de ${cityTempMin} ºC`;
@@ -53,10 +58,11 @@ function mostrarDatosClima(response) {
     "src",
     `https://openweathermap.org/img/wn/${cityWeatherIcon}@4x.png`
   );
-  divDatosClima.appendChild(iconCiudadInfo);
-  divDatosClima.appendChild(cityTempTitulo);
-  divDatosClima.appendChild(ciudadDescInfo);
-  divDatosClima.appendChild(ciudadTitulo);
-  divDatosClima.appendChild(ciudadTempInfo);
-  divDatosClima.appendChild(humedadInfo);
+  divHeaderClima.appendChild(ciudadTitulo);
+  divHeaderClima.appendChild(ciudadDescInfo);
+  divHeaderClima.appendChild(iconCiudadInfo);
+  divHeaderClima.appendChild(cityTempTitulo);
+
+  divFooterClima.appendChild(ciudadTempInfo);
+  divFooterClima.appendChild(humedadInfo);
 }
